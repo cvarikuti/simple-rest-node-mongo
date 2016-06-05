9772240882a676f0f2e30cde428723890c44fbaf
Paul Fremantle <pzfreo@github.com>
2016-06-05 22:37:26 +0100

    initial

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({"extended" : false}));
 
 router.get("/",function(req,res){
-    res.json({"error" : false,"message" : "Hello World"});
+    res.json({"message" : "mongo rest server app"});
 });
 
 router.route("/customers")
