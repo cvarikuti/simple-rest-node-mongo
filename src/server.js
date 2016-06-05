d13c96a4707c1ed6da3e8f3f3e4476a97076ad7e
Paul Fremantle <pzfreo@github.com>
2016-06-05 22:00:57 +0100

    fixed

--- src/server.js
+++ src/server.js
@@ -21,15 +21,25 @@ router.get("/",function(req,res){
 router.route("/customers")
     .get(function(req,res){
 	    Customer.find({}, function(err, data) {
-	    if (!err) res.json(data);
+	    if (!err) {
+	    	for (i in data) {
+	    		delete i._id;
+	    		delete i.__v;
+	    	}
+	    	res.json(data);
+	    }
 	    });
     })
     .post(function(req,res){
 		var c = new Customer(req.body);
 		c.save()
 		Customer.findOne({id:c.id}, function(err, data) {
-			console.log(data);
+			
 			if (!err) {
+				// remove internal fields from Mongo
+				delete data.__v;
+				delete data._id;
+				
 				res.set("Location","/customers/"+c.id);
 				res.status(201).json(data);
 			}
@@ -41,18 +51,26 @@ router.route("/customers/:id")
         var response = {};
         Customer.findOne({id: req.params.id} ,function(err,data){
         // This will run Mongo Query to fetch data based on ID.
-            if(err) {
-                response = {"error" : true,"message" : "Error fetching data"};
-            } else {
-                response = data;
+            if(!err) {
+                // remove internal fields from Mongo
+				delete data.__v;
+				delete data._id;
+				res.json(data);
+                               
             }
-            res.json(response);
+            else {
+            	res.sendStatus(404);
+			}
         });
     })
 
 router.route("/cat")
     .get(function(req,res){
 	    Entry.find({}, function(err, data) {
+	    for (i in data) {
+	    		delete i._id;
+	    		delete i.__v;
+	    	}
 	    if (!err) res.json(data);
 	    });
     })
@@ -61,6 +79,10 @@ router.route("/cat")
 		e.save();
 		Entry.findOne({id:e.id}, function(err, data) {
 			if (!err) {
+				// remove internal fields from Mongo
+				delete data.__v;
+				delete data._id;
+
 				res.set("Location","/cat/"+e.id);
 				res.status(201).json(data);
 			}
@@ -73,12 +95,16 @@ router.route("/cat/:id")
         var response = {};
         Entry.findOne({id: req.params.id} ,function(err,data){
         // This will run Mongo Query to fetch data based on ID.
-            if(err) {
-                response = {"error" : true,"message" : "Error fetching data"};
-            } else {
-                response = data;
+            if(!err) {
+                // remove internal fields from Mongo
+				delete data.__v;
+				delete data._id;
+				res.json(data);
+                               
             }
-            res.json(response);
+            else {
+            	res.sendStatus(404);
+			}
         });
     })
 
