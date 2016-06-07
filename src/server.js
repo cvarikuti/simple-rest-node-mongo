2a509f12e8b9c09bcdab7b536f17c02058942d02
Paul Fremantle <pzfreo@github.com>
2016-06-07 16:04:17 +0100

    missing

--- src/server.js
+++ src/server.js
@@ -19,93 +19,94 @@ app.get("/",function(req,res){
 });
 
 app.get("/customers", function(req,res){
-	    Customer.find({}, function(err, data) {
-	    if (!err && data) {
-	    	for (i in data) {
+	Customer.find({}, function(err, data) {
+		if (!err && data) {
+			for (i in data) {
 				data[i].__v = undefined;
 				data[i]._id = undefined;
-	    	}
-	    	res.json(data);
-	    }
+			}
+			res.json(data);
+		}
 	});
+});
 	    
 	    
 app.post("/customers", function(req,res){
-		var c = new Customer(req.body);
-		c.save()
-		Customer.findOne({id:c.id}, function(err, data) {
-			
-			if (!err && data) {
-				// remove internal fields from Mongo
-				data.__v = undefined;
-				data._id = undefined;
-				
-				res.set("Location","/customers/"+c.id);
-				res.status(201).json(data);
-			}
-		});
+	var c = new Customer(req.body);
+	c.save()
+	Customer.findOne({id:c.id}, function(err, data) {
 		
-    });
+		if (!err && data) {
+			// remove internal fields from Mongo
+			data.__v = undefined;
+			data._id = undefined;
+			
+			res.set("Location","/customers/"+c.id);
+			res.status(201).json(data);
+		}
+	});
+	
+});
 
 app.get("/customers/:id", function(req,res){
-        var response = {};
-        Customer.findOne({id: req.params.id} ,function(err,data){
-        // This will run Mongo Query to fetch data based on ID.
-            if(!err && data) {
-                // remove internal fields from Mongo
-				data.__v = undefined;
-				data._id = undefined;
-				res.json(data); 
-            }
-            else {
-            	res.sendStatus(404);
-			}
-        });
-    });
+	var response = {};
+	Customer.findOne({id: req.params.id} ,function(err,data){
+	// This will run Mongo Query to fetch data based on ID.
+		if(!err && data) {
+			// remove internal fields from Mongo
+			data.__v = undefined;
+			data._id = undefined;
+			res.json(data); 
+		}
+		else {
+			res.sendStatus(404);
+		}
+	});
+});
 
 app.get("/cat", function(req,res){
-	    Entry.find({}, function(err, data) {
-	    for (i in data) {
-				data[i].__v = undefined;
-				data[i]._id = undefined;
-	    	}
-	    if (!err && data) res.json(data);
-	    });
-    });
+	 Entry.find({}, function(err, data) {
+	 for (i in data) {
+			 data[i].__v = undefined;
+			 data[i]._id = undefined;
+		 }
+	 if (!err && data) res.json(data);
+	 });
+ });
     
 
 app.post("/cat", function(req,res){
-		var e = new Entry(req.body);
-		e.save();
-		Entry.findOne({id:e.id}, function(err, data) {
-			if (!err) {
-				// remove internal fields from Mongo
-				data.__v = undefined;
-				data._id = undefined;
-				
-				res.set("Location","/cat/"+e.id);
-				res.status(201).json(data);
-			}
-		});
+	var e = new Entry(req.body);
+	e.save();
+	Entry.findOne({id:e.id}, function(err, data) {
+		if (!err) {
+			// remove internal fields from Mongo
+			data.__v = undefined;
+			data._id = undefined;
+			
+			res.set("Location","/cat/"+e.id);
+			res.status(201).json(data);
+		}
+	});
 
-    });
+});
 
 app.get("/cat/:id", function(req,res){
-        var response = {};
-        Entry.findOne({id: req.params.id} ,function(err,data){
-        // This will run Mongo Query to fetch data based on ID.
-            if(!err && data) {
-                // remove internal fields from Mongo
-				data.__v = undefined;
-				data._id = undefined;
-				res.json(data);
-                               
-            }
-            else {
-            	res.sendStatus(404);
-			}
-        });
-    });
+	var response = {};
+	Entry.findOne({id: req.params.id} ,function(err,data){
+	// This will run Mongo Query to fetch data based on ID.
+		if(!err && data) {
+			// remove internal fields from Mongo
+			data.__v = undefined;
+			data._id = undefined;
+			res.json(data);
+						   
+		}
+		else {
+			res.sendStatus(404);
+		}
+	});
+});
 
 app.listen(8000);
 console.log("Listening to PORT 8000");
